79fa2540f202ab8dd482e7a6c093283b84eb05e4
Antoine Petit <antooine.petit@gmail.com>
2026-02-02 17:56:52 +0100

    feat(recipes): add new appetizer images
    - Updated env variable
    - Added test pictures for front/back connection

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ uploadDirs.forEach((dir) => {
 require("./db");
 
 app.use(cors({
-  origin: process.env.FRONTEND_URL || 'http://localhost:3000',
+  origin: process.env.URL_WEBSITE || 'http://localhost:5173',
   credentials: true
 }));
 app.use(express.json({ limit: "10mb" }));
